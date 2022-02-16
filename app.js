2ede6675816e187d476ddc4f21afbc3d7a8155a9
kévin <kremadnia@gmail.com>
2022-02-16 01:40:15 +0100

    ADD: function for validate phone number

--- app.js
+++ app.js
@@ -59,3 +59,8 @@ function validatePostCode(input) {
 function validateAddress(input) {
   return input.value.match(/^\s*\S+(?:\s+\S+){2}/);
 }
+
+// Validation du Numéro de téléphone
+function validatePhoneNumber(input) {
+  return input.value.match(/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/);
+}
