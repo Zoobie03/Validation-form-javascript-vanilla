14b809868e2a277a9d6f905240eacda2a396e047
kévin <kremadnia@gmail.com>
2022-02-16 01:35:53 +0100

    ADD: function for validate text length field

--- app.js
+++ app.js
@@ -4,4 +4,34 @@
   // Déclaration du formulaire
   let form = document.getElementById('myForm');
 
+  // Ecoute de l'action submit, et lancement de la fonction
+  form.addEventListener('submit', function(event) {
+    Array.from(form.elements).forEach(input => {
+      if (input.type !== 'submit') {
+        if (!validateFields(input)) {
+          event.preventDefault();
+          event.stopPropagation();
+
+          input.classList.remove('is-valid');
+          input.classList.add('is-invalid');
+          input.nextElementSibling.style.display = 'block';
+        } else {
+          input.nextElementSibling.style.display = 'none';
+          input.classList.remove('is-invalid');
+          input.classList.add('is-valid');
+        }
+      }
+    });
+  }, false);
+
 })();
+
+// Validation d'un champ REQUIRED
+function validateRequired(input) {
+  return !(input.value == null || input.value == "");
+}
+
+// Validation du nombres de caractères: MIN & MAX
+function validateLength(input, minLength, maxLength) {
+  return !(input.value.length < minLength || input.value.length > maxLength);
+}
